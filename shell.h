0226a2f3117d96e425c532a208316578e28dfd3f
abde109 <abderrahimkhadri2@gmail.com>
2023-07-27 11:15:46 +0000

    final betty

--- shell.h
+++ shell.h
@@ -17,7 +17,7 @@
 /* Data structures */
 
 /*
- * struct list_s - linked list for storing strings
+ * struct @list_s - linked list for storing strings
  * @str: string stored in the node
  * @next: pointer to the next node in the list
  */
@@ -28,7 +28,7 @@ typedef struct list_s
 } list_t;
 
 /*
- * struct info_s - struct to store environment information
+ * struct @info_s - struct to store environment information
  * @env: pointer to the first node in the list of environment variables
  */
 typedef struct info_s
@@ -37,7 +37,7 @@ typedef struct info_s
 } info_t;
 
 /*
- * struct ShellState - struct to store shell state
+ * struct @ShellState - struct to store shell state
  * @exitStatus: exit status of the shell
  * @line_count: current line count in the shell
  */
