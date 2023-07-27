282a52ff66600719525f0cb52e6c348322ac9a3e
abde109 <abderrahimkhadri2@gmail.com>
2023-07-27 10:36:15 +0000

    final betty

--- exit_ops.c
+++ exit_ops.c
@@ -11,6 +11,7 @@ void exit_shell(void)
 /**
  * exit_shell_status - Exits the shell with a status.
  * @parameters: The parameters for exit status.
+ * @state: The current state of the shell.
  */
 void exit_shell_status(char **parameters, ShellState *state)
 {
